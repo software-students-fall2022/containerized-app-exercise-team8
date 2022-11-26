6b94d19cf21e76b89bfaec4ff42e7e35c7fed950
shannonh800 <sh5572@stern.nyu.edu>
2022-11-25 20:12:13 -0500

    Implemented static file rendering and basic home page setup

--- web-app/app.py
+++ web-app/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def sentimentGuesser():
-    return "<p>Press the microphone button and start speaking!</p>"
+    return render_template("homePage.html")
 
