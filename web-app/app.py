c02437996b6e75e0d65740fb763d4ad6cdcb6d1d
ishana-goyal <88641809+ishana-goyal@users.noreply.github.com>
2022-11-26 18:07:33 -0500

    adding audio input functionality to front-end, does not work entirely

--- web-app/app.py
+++ web-app/app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import speech_recognition as sr
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def sentimentGuesser():
+    #file = request.files["file"]
+    # recognizer = sr.Recognizer()
+
+    # with sr.Microphone() as source:
+    #     print("Listening!")
+    #     audio = recognizer.listen(source)
+    #     try:
+    #         transcript = recognizer.recognize_google(audio, key = None)
+    #     except:
+    #         transcript = None
+    #         print("Could not transcribe")
+
+    # return render_template("homePage.html", transcript = transcript)
     return render_template("homePage.html")
 
