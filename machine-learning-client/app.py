9f43ff20a8eee84bc6ed1718c97fd41b4f5c8d11
ankit181818 <75585933+ankit181818@users.noreply.github.com>
2022-12-01 17:57:58 -0500

    made a working front end for ml client

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -5,31 +5,29 @@ import os
 from dotenv import dotenv_values
 import pymongo
 
+ALLOWED_EXTENSIONS = {"wav", "WAV"} #can optionally add more once we test our modules further
 
+# # load credentials and configuration options from .env file
+# # if you do not yet have a file named .env, make one based on the template in env.example
+# config = dotenv_values(".env")
 
-# load credentials and configuration options from .env file
-# if you do not yet have a file named .env, make one based on the template in env.example
-config = dotenv_values(".env")
+# # turn on debugging if in development mode
+# if config['FLASK_ENV'] == 'development':
+#     # turn on debugging, if in development
+#     app.debug = True # debug mnode
 
-# turn on debugging if in development mode
-if config['FLASK_ENV'] == 'development':
-    # turn on debugging, if in development
-    app.debug = True # debug mnode
-
-# connect to the database
-cxn = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
-try:
-    # verify the connection works by pinging the database
-    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
-    db = cxn[config['MONGO_DBNAME']] # store a reference to the database
-    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
-except Exception as e:
-    # the ping command failed, so the connection is not available.
-    # render_template('error.html', error=e) # render the edit template
-    print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
-    print('Database connection error:', e) # debug
-
-
+# # connect to the database
+# cxn = pymongo.MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
+# try:
+#     # verify the connection works by pinging the database
+#     cxn.admin.command('ping') # The ping command is cheap and does not require auth.
+#     db = cxn[config['MONGO_DBNAME']] # store a reference to the database
+#     print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+# except Exception as e:
+#     # the ping command failed, so the connection is not available.
+#     # render_template('error.html', error=e) # render the edit template
+#     print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
+#     print('Database connection error:', e) # debug
 
 
 app = Flask(__name__)
@@ -46,6 +44,13 @@ def index():
     else:
         return render_template("index.html")
 
+@app.route("/upload", methods = ['POST', 'GET'])
+def upload():
+    if request.method == "POST":
+        return render_template('upload.html')
+    else:
+        return render_template('index.html')
+
 
 # phrase can be a list input of space-separated words said by user, parsed by us
 # audio can be the audio file recorded by the front end
@@ -60,28 +65,19 @@ def getForm(form):
     # overall function to parse entries by the user
     # uses parse_phrase_from_voice() and check_new_user()
     return
+
 def post_add_record():
     # function to save a user's formatted input and sentiment to the db
-    return
-def post_delete_record():
-    # function to delete a record from the db
-    # form = request.form
-    # print(form)
-    # db.songs.delete_one({
-    #     '_id': ObjectId(form['mongoId'])
-    # })
     return
 
 def parse_phrase_from_voice(audio):
     #takes audio input and generates a phrase list from it using ML
     return
+
 def calculate_sentiment(phrase):
     #calculate the sentiment associated with a phrase input
     return
-def update_summary_stats():
-    #create summary stats based off of all the data in the database
-    # we can have things like average sentiment, most popular word, etc.
-    return
+
 def check_new_user(user_name):
     # return a boolean representing whether the user is new or existing, will impact how we update the db
     return
