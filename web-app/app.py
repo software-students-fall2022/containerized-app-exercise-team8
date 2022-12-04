2681ad3726b77dbca95411ba65c5d8bf48ec0c26
shannonh800 <sh5572@stern.nyu.edu>
2022-12-04 15:02:36 -0500

    Updated web app get and post pages, db part still needs work

--- web-app/app.py
+++ web-app/app.py
@@ -1,22 +1,17 @@
 from flask import Flask, render_template, request
-import speech_recognition as sr
+import pymongo
+
+# set up connection to database & call the database "db"
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET', 'POST'])
-def sentimentGuesser():
-    #file = request.files["file"]
-    # recognizer = sr.Recognizer()
-
-    # with sr.Microphone() as source:
-    #     print("Listening!")
-    #     audio = recognizer.listen(source)
-    #     try:
-    #         transcript = recognizer.recognize_google(audio, key = None)
-    #     except:
-    #         transcript = None
-    #         print("Could not transcribe")
-
-    # return render_template("homePage.html", transcript = transcript)
-    return render_template("homePage.html")
-
+@app.route("/", methods = ["GET", "POST"])
+def displaySavedResults():
+    if(request.method == "GET"):
+        return render_template("enterUserInfo.html")
+    else:
+        userName = request.args.userName
+        userDocument = db.sentiment_analyzer.find({'user_name':userName})[0]
+        transcribedAudio = userDocument.transcribed_audio
+        sentiment = userDocument.sentiment
+        return render_template("displayUserResults.html", userName=userName, transcribedAudio=transcribedAudio, sentiment=sentiment)
