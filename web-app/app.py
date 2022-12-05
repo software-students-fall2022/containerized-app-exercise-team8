566c113c6444766ec7f5b7827e1d225617f3fb2f
shannonh800 <sh5572@stern.nyu.edu>
2022-12-04 20:50:01 -0500

    Updated db connection code

--- web-app/app.py
+++ web-app/app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, request
-import pymongo
+from pymongo import MongoClient
 
 # set up connection to database & call the database "db"
+# # connect to the database
+cxn = MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
+try:
+    # verify the connection works by pinging the database
+    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
+    db = cxn[config['MONGO_DBNAME']] # store a reference to the database
+    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+except Exception as e:
+    # the ping command failed, so the connection is not available.
+    render_template('error.html', error=e) # render the edit template
+    print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
+    print('Database connection error:', e) # debug
 
 app = Flask(__name__)
 
