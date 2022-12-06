481b0129d8ec4e115bf1a93720b285df6acb7a94
graces <gracezhang89gz@gmail.com>
2022-12-06 14:09:15 -0500

    Changed from app import app

--- machine-learning-client/tests/test.py
+++ machine-learning-client/tests/test.py
@@ -1,6 +1,7 @@
 import pytest
 import sys
-from app import app
+import app
+
 
 class Tests:
 
@@ -17,10 +18,13 @@ class Tests:
         assert actual == expected, "Expected True to be equal to True!"
 
     def test_parse_phrase_from_voice_returntype(self):
-        assert isinstance(app.parse_phrase_from_voice("machine-learning-client/Sample-text-for-SWE.wav"), str), "did not return transcribed audio as string"
+        assert isinstance(app.parse_phrase_from_voice(
+            "machine-learning-client/Sample-text-for-SWE.wav"), str), "did not return transcribed audio as string"
 
     def test_calculate_sentiment_returntype(self):
-        assert isinstance(app.calculate_sentiment("hello, this is a test phrase"), dict), "did not return dictionary with sentiment analysis"
+        assert isinstance(app.calculate_sentiment("hello, this is a test phrase"),
+                          dict), "did not return dictionary with sentiment analysis"
 
     def test_check_new_user_returntype(self):
-        assert isinstance(app.check_new_user("xyz123"), bool), "did not return boolean value"
+        assert isinstance(app.check_new_user("xyz123"),
+                          bool), "did not return boolean value"
