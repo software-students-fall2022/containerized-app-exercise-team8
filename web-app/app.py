0f3f40537841b2d23513e08664b390d2b0c1abd3
graces <gracezhang89gz@gmail.com>
2022-12-05 20:13:56 -0500

    Docker works

--- web-app/app.py
+++ web-app/app.py
@@ -6,30 +6,39 @@ from dotenv import dotenv_values
 # # connect to the database
 config = dotenv_values(".env")
 
-cxn = MongoClient(config['MONGO_URI'], serverSelectionTimeoutMS=5000)
-try:
-    # verify the connection works by pinging the database
-    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
-    db = cxn[config['MONGO_DBNAME']] # store a reference to the database
-    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
-except Exception as e:
-    # the ping command failed, so the connection is not available.
-    render_template('error.html', error=e) # render the edit template
-    print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
-    print('Database connection error:', e) # debug
-
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
+MONGO_URI = "mongodb://mongo:27017"
+COLLECTION_NAME = 'sentiment_analyzer'
+
+try:
+    client = MongoClient(
+        MONGO_URI, serverSelectionTimeoutMS=30000, username="root", password="example")
+    db = client[config['MONGO_DBNAME']]
+    collection = client[config['MONGO_DBNAME']][COLLECTION_NAME]
+    # if we get here, the connection worked!
+    print(' *', 'Connected to MongoDB!')
+except Exception as e:
+    # the ping command failed, so the connection is not available.
+    render_template('error.html', error=e)  # render the edit template
+    print(' *', "Failed to connect to MongoDB at", config['MONGO_URI'])
+    print('Database connection error:', e)  # debug
+
+
+@app.route("/", methods=["GET", "POST"])
 def displaySavedResults():
-    if(request.method == "GET"):
+    if request.method == "GET":
         return render_template("enterUserInfo.html")
     else:
         userName = request.form["userName"]
-        userDocument = db.sentiment_analyzer.find_one({'user_name':userName})
+        userDocument = db.COLLECTION_NAME.find_one({'user_name': userName})
         if userDocument is None:
             return render_template("displayUserResults.html", nothingFound="Sorry :( no record saved for this user!")
         else:
             transcribedAudio = userDocument["transcribed_audio"]
             sentiment = userDocument["sentiment"]
             return render_template("displayUserResults.html", somethingFound=1, userName=userName, transcribedAudio=transcribedAudio, sentiment=sentiment)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=2001, debug=True)
